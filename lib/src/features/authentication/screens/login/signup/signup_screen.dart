8221c13b2bccfa4f0d863f270453200aa0d32924
leohcalcanti <leonardocaturyty@gmail.com>
2023-05-07 14:57:10 -0300

    Starting the registration screen

---
+++ lib/src/features/authentication/screens/login/signup/signup_screen.dart
@@ -0,0 +1,22 @@
+import 'package:arle_app/src/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(tDefaultSize),
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
