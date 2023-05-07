47988b6d9c35c8b0e5b20a8a483c9ca2eb7cebdb
leohcalcanti <leonardocaturyty@gmail.com>
2023-05-07 17:28:31 -0300

    full form

--- lib/src/features/authentication/screens/login/signup/widgets/signup_form_widget.dart
+++ lib/src/features/authentication/screens/login/signup/widgets/signup_form_widget.dart
@@ -10,60 +10,64 @@ class SignUpForm extends StatelessWidget {
     return Form(
       child: Container(
         padding: EdgeInsets.symmetric(vertical: tFromHeight - 10),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                    label: Text("Full Name"),
-                    prefixIcon: Icon(
-                      Icons.person_outline_rounded,
-                      color: tPrimaryColor,
-                    ),
-                    border: OutlineInputBorder(),
-                    labelStyle: TextStyle(color: tPrimaryColor),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 2.0, color: tPrimaryColor))),
-              ),
-              const SizedBox(
-                height: tFromHeight - 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    label: Text("E-mail"),
-                    prefixIcon: Icon(
-                      Icons.email_outlined,
-                      color: tPrimaryColor,
-                    ),
-                    border: OutlineInputBorder(),
-                    labelStyle: TextStyle(color: tPrimaryColor),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 2.0, color: tPrimaryColor))),
-              ),
-              const SizedBox(
-                height: tFromHeight - 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    label: Text("Password"),
-                    prefixIcon: Icon(
-                      Icons.fingerprint,
-                      color: tPrimaryColor,
-                    ),
-                    border: OutlineInputBorder(),
-                    labelStyle: TextStyle(color: tPrimaryColor),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 2.0, color: tPrimaryColor)),
-                    suffixIcon: IconButton(
-                        onPressed: null,
-                        icon: Icon(Icons.remove_red_eye_sharp))),
-              ),
-            ],
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                  label: Text("Full Name"),
+                  prefixIcon: Icon(
+                    Icons.person_outline_rounded,
+                    color: tPrimaryColor,
+                  ),
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(color: tPrimaryColor),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 2.0, color: tPrimaryColor))),
+            ),
+            const SizedBox(
+              height: tFromHeight - 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  label: Text("E-mail"),
+                  prefixIcon: Icon(
+                    Icons.email_outlined,
+                    color: tPrimaryColor,
+                  ),
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(color: tPrimaryColor),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 2.0, color: tPrimaryColor))),
+            ),
+            const SizedBox(
+              height: tFromHeight - 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  label: Text("Password"),
+                  prefixIcon: Icon(
+                    Icons.fingerprint,
+                    color: tPrimaryColor,
+                  ),
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(color: tPrimaryColor),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 2.0, color: tPrimaryColor)),
+                  suffixIcon: IconButton(
+                      onPressed: null, icon: Icon(Icons.remove_red_eye_sharp))),
+            ),
+            const SizedBox(
+              height: tFromHeight - 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {}, child: Text("Login".toUpperCase())),
+            ),
+          ],
         ),
       ),
     );
