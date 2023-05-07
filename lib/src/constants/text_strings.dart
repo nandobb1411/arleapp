3a19bbed7259ae4a1ce96219ad2b67a1eacf9fe8
leohcalcanti <leonardocaturyty@gmail.com>
2023-05-07 14:28:06 -0300

    additions of new text

--- lib/src/constants/text_strings.dart
+++ lib/src/constants/text_strings.dart
@@ -8,4 +8,9 @@ const String tWelcomeSubTitle = "Let's put your creativity on the development hi
 const String tLoginTitle = "Welcome Back,";
 const String tLoginSubTitle = "Make it work, make it right, make it fast.";
 const String tRememberMe = "Remember Me?";
-const String tAlreadyHaveAnAccount = "Already have an account?";
+const String tDontHaveAnAccount = "Already have an account?";
+
+// -- Sign Up Screen Text
+const String tSignUpTitle = "Get On Board!";
+const String tSignUpSubTitle = "Create your profile to start your Journey with us...";
+const String tAlreadyHaveAnAccount = "Already have an Account?";
